da02bad9902fa2922145c3cb83032847e6c65199
ryanwbachman <ryanwbachman@gmail.com>
2017-09-13 22:47:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,14 +68,14 @@ function endGame() {
 }
 
 function moveDodger(e) {
+
+  e.preventDefault;
+  e.stopPropagation;
+
   if(e.which === LEFT_ARROW){
-    e.preventDefault;
-    e.stopPropagation;
     moveDodgerLeft();
   }
   if(e.which === RIGHT_ARROW){
-    e.preventDefault;
-    e.stopPropagation;
     moveDodgerRight();
   }
 }
