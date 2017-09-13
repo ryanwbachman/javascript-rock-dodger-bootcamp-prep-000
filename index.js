351a7fbc4f81f19558a04d2ef94e0e9f833834e2
ryanwbachman <ryanwbachman@gmail.com>
2017-09-13 21:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  window.requestAnimationFrame(function(){}
+  window.requestAnimationFrame(function(){
     var leftNumbers = dodger.style.left.replace('px','');
     var left = parseInt(leftNumbers, 10);
 
@@ -161,7 +161,7 @@ function start() {
   window.addEventListener('keydown', moveDodger)
 
   START.style.display = 'none'
-  
+
   gameInterval = setInterval(function() {
     createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
   }, 1000)
