cbb72450fbc6c5adbcf3e1081e8e211c3f5d2e40
ryanwbachman <ryanwbachman@gmail.com>
2017-09-13 22:51:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,8 +69,17 @@ function endGame() {
 
 function moveDodger(e) {
 
+
+  const code = e.which
+
+    if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
+      e.preventDefault()
+      e.stopPropagation()
+    }
+    
   e.preventDefault;
   e.stopPropagation;
+
 
   if(e.which === LEFT_ARROW){
     moveDodgerLeft();
